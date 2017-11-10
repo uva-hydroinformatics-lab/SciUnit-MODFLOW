e154777124e4dc3dac9e501b12119d83b923e6ed
edwmorgan <edwardm72@gmail.com>
2017-11-10 00:51:28 -0500

    Splitting up installation and running of MODFLOW2005

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Installs MODFLOW2005
+# Should be done only once to install MODFLOW2005 in the SciUnit. MODFLOW2005 is run using modflow.sh
+
+cd ~/Desktop
+
+echo "Downloading MODFLOW2005.."
+wget --no-check-certificate https://water.usgs.gov/ogw/modflow/MODFLOW-2005_v1.12.00/MF2005.1_12u.zip
+unzip MF2005.1_12u.zip
+rm MF2005.1_12u.zip
+echo "Installation done!"
+
+# Compiling MODFLOW2005
+cd MF2005.1_12u/make
+echo "Compiling MODFLOW2005..."
+make -f makefile
--- modflow.sh
+++ modflow.sh
@@ -5,42 +5,20 @@
 #apt-get update
 #apt-get install -y --no-install-recommends make unzip wget gfortran 
 
-cd ~/Desktop
-
-# If it's already installed, don't install it again
-if [ $(find ~/Desktop/MF2005.1_12u/make/ -name mf2005) ]
-then
-    echo "MODFLOW2005 already installed on ~/Desktop."
-    echo "Skipping installation..."
-else
-    echo "Downloading MODFLOW2005.."
-    wget https://water.usgs.gov/ogw/modflow/MODFLOW-2005_v1.12.00/MF2005.1_12u.zip
-    unzip MF2005.1_12u.zip
-    rm MF2005.1_12u.zip
-    echo "Installation done!"
-fi
-
-# Compiling MODFLOW2005
-cd MF2005.1_12u/make
-echo "Compiling MODFLOW2005..."
-make -f makefile
-
-cd ~/Desktop
-
-# Getting the directory name from the path to the .nam file
-#MODFLOWdir="$(echo $1 | sed 's/\/[^\/]*\.nam//')"
-echo $1
-# Switching to the directory where the files are
-#cd ${MODFLOWdir}
-
-#echo "Running MODFLOW2005 using the files in `pwd`"
+# Getting the MI resource
 wget https://www.hydroshare.org/django_irods/download/bags/$1.zip
 
 unzip $1.zip
 
-cd ~/Desktop/$1/data/contents
+cd $1/data/contents
 
+# Running MODFLOW2005 on the .nam file in the MI resource
 
-~/Desktop/MF2005.1_12u/make/mf2005 $(find -iregex .*".nam")
+# Finding where the mf2005 executable is in the sciunit
+# This could return a mf2005 executable in a different sciunit, however...
+cd ../../..
+modflowrun=`find . -path *cde-root/*/make/mf2005 -type f | head -1`
+echo "modflowrun: $modflowrun"
+$modflowrun `find -iregex .*".nam"`
 
 
